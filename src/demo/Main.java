aa2483202b9be2143b10efe8ae3a6a2e6c254c64
JJAV <info@impactotecnologico.net>
2018-09-25 08:56:28 +0200

    first commit

---
+++ src/demo/Main.java
@@ -0,0 +1,12 @@
+package demo;
+
+import java.time.Instant;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hola Mundo ahora: " + Instant.now());
+
+  }
+
+}
